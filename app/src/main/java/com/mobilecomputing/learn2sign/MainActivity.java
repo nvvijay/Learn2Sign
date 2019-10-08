c4b3495a8ec1c0c38dc6fc67ce14de66402bdc56
nvvijay <19.nvv.11@gmail.com>
2019-10-07 22:09:50 -0700

    Initial commit after java migration

---
+++ app/src/main/java/com/mobilecomputing/learn2sign/MainActivity.java
@@ -0,0 +1,60 @@
+package com.mobilecomputing.learn2sign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean isSpinnerTouched = false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.word_arrays, R.layout.word_spinner_item);
+        adapter.setDropDownViewResource(R.layout.word_droopdown_item);
+        Spinner spinner = findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+
+
+        // without this, spinner will get selected automatically
+        spinner.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                isSpinnerTouched = true;
+                return false;
+            }
+        });
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> arg0, View arg1,
+                                       int arg2, long arg3) {
+                String str = (String) arg0.getSelectedItem();
+
+                //here print selected value...
+                System.out.println("String is :: " + str);
+
+                //And StartActivity here...
+
+                if(isSpinnerTouched){
+                    Intent intent = new Intent(MainActivity.this,PlayHelpVideo.class);
+                    intent.putExtra("sign", str);
+                    startActivity(intent);
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/mobilecomputing/learn2sign/PlayHelpVideo.java
@@ -0,0 +1,97 @@
+package com.mobilecomputing.learn2sign;
+
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class PlayHelpVideo extends AppCompatActivity {
+    private VideoView myVideoView;
+    private int position = 0;
+    private ProgressDialog progressDialog;
+    private MediaController mediaControls;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_help_video);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // go to record video here
+            }
+        });
+
+        //set the media controller buttons
+        if (mediaControls == null) {
+            mediaControls = new MediaController(PlayHelpVideo.this);
+        }
+
+        //initialize the VideoView
+        myVideoView = (VideoView) findViewById(R.id.videoview);
+
+        // create a progress bar while the video file is loading
+        progressDialog = new ProgressDialog(PlayHelpVideo.this);
+        // set a title for the progress bar
+        progressDialog.setTitle("Loading Video");
+        // set a message for the progress bar
+        progressDialog.setMessage("Loading...");
+        //set the progress bar not cancelable on users' touch
+        progressDialog.setCancelable(true);
+        // show the progress bar
+        progressDialog.show();
+
+        try {
+            //set the media controller in the VideoView
+            myVideoView.setMediaController(mediaControls);
+
+            //set the uri of the video to be played
+            myVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/raw/explain"));
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        myVideoView.requestFocus();
+        //we also set an setOnPreparedListener in order to know when the video file is ready for playback
+        myVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                // close the progress bar and play the video
+                progressDialog.dismiss();
+                //if we have a position on savedInstanceState, the video playback should start from here
+                myVideoView.seekTo(position);
+                if (position == 0) {
+                    myVideoView.start();
+                } else {
+                    //if we come from a resumed activity, video playback will be paused
+                    myVideoView.pause();
+                }
+            }
+        });
+
+        myVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                myVideoView.start();
+            }
+        });
+    }
+
+}
